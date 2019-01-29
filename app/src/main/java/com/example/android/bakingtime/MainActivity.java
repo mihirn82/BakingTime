414682188cdb85abc32279017c3faf0af97ff478
Prathamesh Shetye <prathamesh.shetye@gmail.com>
2019-01-28 19:20:33 -0800

    Fixed RecyclerView and Click events

--- app/src/main/java/com/example/android/bakingtime/MainActivity.java
+++ app/src/main/java/com/example/android/bakingtime/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.IdlingResource;
 import android.os.Bundle;
@@ -23,12 +24,13 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.bakingtime.IdlingResource.SimpleIdlingResource;
+import com.example.android.bakingtime.RecipesAdapter.RecipeCardClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
-        implements LoaderCallbacks {
+        implements LoaderCallbacks<List<Recipes>>, RecipeCardClickListener {
 
     /** URL for baking data from the baking app dataset */
     private static final String RECIPE_REQUEST_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
@@ -51,9 +53,6 @@ public class MainActivity extends AppCompatActivity
 
     private ProgressBar mProgress;
 
-//    private ListView recipesListView = null;
-    private RecyclerView recyclerView = null;
-
     @Nullable
     private SimpleIdlingResource mIdlingResource;
 
@@ -74,35 +73,17 @@ public class MainActivity extends AppCompatActivity
         getIdlingResource();
 
         // Find a reference to the {@link ListView} in the layout
-//        recipesListView = (ListView) findViewById(R.id.list_view_recipes);
-        recyclerView = (RecyclerView) findViewById(R.id.list_view_recipes);
+        RecyclerView recyclerView = findViewById(R.id.list_view_recipes);
 
-        mProgress = (ProgressBar) findViewById(R.id.ProgressBar);
+        mProgress = findViewById(R.id.ProgressBar);
 
-        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
-//        recipesListView.setEmptyView(mEmptyStateTextView);
+        mEmptyStateTextView = findViewById(R.id.empty_view);
 
         // Create a new {@link ArrayAdapter} of recipes
-        mAdapter = new RecipesAdapter(this, new ArrayList<Recipes>());
-
-//        recipesListView.setAdapter(mAdapter);
+        mAdapter = new RecipesAdapter();
         recyclerView.setAdapter(mAdapter);
-
-//        recipesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-//                Intent intent = new Intent (MainActivity.this, RecipeActivity.class);
-//
-//                Recipes currentRecipe = mAdapter.getItem(position);
-//                Log.i(LOG_TAG,"Id = " + currentRecipe.getId());
-//                Log.i(LOG_TAG,"Name = " + currentRecipe.getName());
-//
-//                intent.putExtra(KEY_RECIPE,currentRecipe);
-//
-//                startActivity(intent);
-//            }
-//        });
-
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter.setOnItemClickListener(this);
 
         // Get a reference to the LoaderManager, in order to interact with loaders.
         LoaderManager loaderManager = getLoaderManager();
@@ -124,33 +105,44 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public Loader onCreateLoader(int id, Bundle args) {
+    public Loader<List<Recipes>> onCreateLoader(int id, Bundle args) {
         Log.v(LOG_TAG,"Inside onCreateLoader");
         Uri baseUri = Uri.parse(RECIPE_REQUEST_URL);
         Uri.Builder uriBuilder = baseUri.buildUpon();
         return new RecipesLoader(this, uriBuilder.toString());
-
-
     }
 
     @Override
-    public void onLoadFinished(Loader loader, Object data) {
+    public void onLoadFinished(Loader<List<Recipes>> loader, List<Recipes> data) {
         Log.v(LOG_TAG,"Inside onLoadFinished");
 
         mProgress.setVisibility(View.GONE);
 
-        // Set empty state text to display "No recipes found."
-        mEmptyStateTextView.setText(R.string.no_recipes);
+        if (data.isEmpty()) {
+            // Set empty state text to display "No recipes found."
+            mEmptyStateTextView.setText(R.string.no_recipes);
+        }
 
-        mAdapter.swapData((List<Recipes>) data);
-
+        mAdapter.swapData(data);
     }
 
     @Override
     public void onLoaderReset(Loader loader) {
-
         Log.v(LOG_TAG,"Inside onLoaderReset");
 
-//        mAdapter.clear();
+        mAdapter.swapData(new ArrayList<>());
+    }
+
+    @Override
+    public void onRecipeCardClicked(View view, int position) {
+        Intent intent = new Intent (this, RecipeActivity.class);
+
+        Recipes currentRecipe = mAdapter.getItem(position);
+        Log.i(LOG_TAG,"Id = " + currentRecipe.getId());
+        Log.i(LOG_TAG,"Name = " + currentRecipe.getName());
+
+        intent.putExtra(KEY_RECIPE,currentRecipe);
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/android/bakingtime/RecipesAdapter.java
+++ app/src/main/java/com/example/android/bakingtime/RecipesAdapter.java
@@ -1,17 +1,15 @@
 package com.example.android.bakingtime;
 
-import android.app.Activity;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,63 +21,15 @@ import butterknife.ButterKnife;
 //public class RecipesAdapter extends ArrayAdapter<Recipes> {
 public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHolder> {
 
-    private static final String LOG_TAG = RecipesAdapter.class.getSimpleName();
+    private static final String LOG_TAG = "RecipesAdapter";
 
-    private List<Recipes> mRecipesList;
-    private Context context;
+    private List<Recipes> mRecipesList = new ArrayList<>();
+
+    /**
+     * Since you have just one view type in the recycler view, set this in the constructor and pass it along to
+     * every adapter
+     */
     private RecipeCardClickListener listener;
-
-    /**
-     * This is our own custom constructor (it doesn't mirror a superclass constructor).
-     * The context is used to inflate the layout file, and the list is the data we want
-     * to populate into the lists.
-     *
-     * @param context        The current context. Used to inflate the layout file.
-     * @param recipes A List of news objects to display in a list
-     */
-    public RecipesAdapter(Context context, ArrayList<Recipes> recipes) {
-        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
-        // the second argument is used when the ArrayAdapter is populating a single TextView.
-        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
-        // going to use this second argument, so it can be any value. Here, we used 0.
-//        super(context, 0, recipes);
-        this.context = context;
-        this.mRecipesList = recipes;
-    }
-
-    /**
-     * Provides a view for an AdapterView (ListView, GridView, etc.)
-     *
-     * @param position The position in the list of data that should be displayed in the
-     *                 list item view.
-     * @param convertView The recycled view to populate.
-     * @param parent The parent ViewGroup that is used for inflation.
-     * @return The View for the position in the AdapterView.
-     */
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        // Check if the existing view is being reused, otherwise inflate the view
-//        View listItemView = convertView;
-//        if(listItemView == null) {
-//            listItemView = LayoutInflater.from(getContext()).inflate(
-//                    R.layout.list_item, parent, false);
-//        }
-//
-//        // Get the {@link AndroidFlavor} object located at this position in the list
-//        Recipes currentRecipes = getItem(position);
-//
-//        TextView nameView = (TextView) listItemView.findViewById(R.id.recipe_name_tv);
-//        String sName = currentRecipes.getName();
-//        nameView.setText(sName);
-//
-//        TextView servingsView = (TextView) listItemView.findViewById(R.id.servings_tv);
-//        int sServings = currentRecipes.getServings();
-//        servingsView.setText(Integer.toString(sServings));
-//
-//        // Return the whole list item layout
-//        // so that it can be shown in the ListView
-//        return listItemView;
-//    }
 
     void swapData(List<Recipes> recipes) {
         mRecipesList.clear();
@@ -87,22 +37,19 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
         notifyDataSetChanged();
     }
 
+    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.list_item,parent,false);
-        return new ViewHolder(view);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.list_item,
+                parent,
+                false
+        ));
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecipesAdapter.ViewHolder holder, int position) {
-        String name = mRecipesList.get(position).getName();
-        int servings = mRecipesList.get(position).getServings() ;
-        int recipeId = Integer.valueOf(mRecipesList.get(position).getId());
-
-
-        holder.name.setText(name);
-//        holder.servings.setText(context.getString(R.string.servings, servings));
+        holder.bind(mRecipesList.get(position));
     }
 
 
@@ -113,18 +60,37 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        @BindView(R.id.recipe_name_tv)TextView name;
-        @BindView(R.id.servings_tv)TextView servings;
+        private TextView name;
+        private TextView servings;
 
-        public ViewHolder(View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
+            name = itemView.findViewById(R.id.recipe_name_tv);
+            servings = itemView.findViewById(R.id.servings_tv);
             itemView.setOnClickListener(this);
-            ButterKnife.bind(this,itemView);
+            ButterKnife.bind(this, itemView);
         }
 
         @Override
         public void onClick(View view) {
-            listener.onRecipeCardClicked(view, getAdapterPosition());
+            if (listener != null) {
+                listener.onRecipeCardClicked(view, getAdapterPosition());
+            }
+        }
+
+        void bind(Recipes recipes) {
+            name.setText(recipes.getName());
+            String servingsText = servings.getContext().getString(R.string.servings, Integer.valueOf(recipes.getId()));
+            servings.setText(servingsText);
+        }
+    }
+
+    @Nullable
+    public Recipes getItem(int position) {
+        if (position > mRecipesList.size() || position < 0) {
+            return null;
+        } else {
+            return mRecipesList.get(position);
         }
     }
 
@@ -142,5 +108,7 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
     /**
      * Custom click listener
      */
-    public void setOnItemClickListener(RecipeCardClickListener listener){this.listener = listener;}
+    public void setOnItemClickListener(RecipeCardClickListener listener) {
+        this.listener = listener;
+    }
 }
