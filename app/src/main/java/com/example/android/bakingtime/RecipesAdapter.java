cfac751c5b7d196f086a718d315d8a0e7ebd4c74
mihirnewalkar <mihirn82@gmail.com>
2019-01-30 06:47:26 -0600

    Recycler View changes final

--- app/src/main/java/com/example/android/bakingtime/RecipesAdapter.java
+++ app/src/main/java/com/example/android/bakingtime/RecipesAdapter.java
@@ -1,19 +1,14 @@
 package com.example.android.bakingtime;
 
-import android.app.Activity;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -23,11 +18,11 @@ import butterknife.ButterKnife;
 //public class RecipesAdapter extends ArrayAdapter<Recipes> {
 public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHolder> {
 
-    private static final String LOG_TAG = RecipesAdapter.class.getSimpleName();
+//    private static final String LOG_TAG = RecipesAdapter.class.getSimpleName();
+    private static final String LOG_TAG = "RecipesAdapter";
 
-    private List<Recipes> mRecipesList;
-    private Context context;
-    private RecipeCardClickListener listener;
+//    private List<Recipes> mRecipesList;
+    private List<Recipes> mRecipesList = new ArrayList<>();
 
     /**
      * This is our own custom constructor (it doesn't mirror a superclass constructor).
@@ -37,15 +32,14 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
      * @param context        The current context. Used to inflate the layout file.
      * @param recipes A List of news objects to display in a list
      */
-    public RecipesAdapter(Context context, ArrayList<Recipes> recipes) {
-        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
-        // the second argument is used when the ArrayAdapter is populating a single TextView.
-        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
-        // going to use this second argument, so it can be any value. Here, we used 0.
+//    public RecipesAdapter(Activity context, ArrayList<Recipes> recipes) {
+//        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+//        // the second argument is used when the ArrayAdapter is populating a single TextView.
+//        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+//        // going to use this second argument, so it can be any value. Here, we used 0.
 //        super(context, 0, recipes);
-        this.context = context;
-        this.mRecipesList = recipes;
-    }
+//        mRecipesList = recipes;
+//    }
 
     /**
      * Provides a view for an AdapterView (ListView, GridView, etc.)
@@ -81,30 +75,28 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
 //        return listItemView;
 //    }
 
+    private RecipeCardClickListener listener;
+
     void swapData(List<Recipes> recipes) {
         mRecipesList.clear();
         mRecipesList.addAll(recipes);
         notifyDataSetChanged();
     }
 
+    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.list_item,parent,false);
-        return new ViewHolder(view);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.list_item,
+                parent,
+                false
+        ));
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecipesAdapter.ViewHolder holder, int position) {
-        String name = mRecipesList.get(position).getName();
-        int servings = mRecipesList.get(position).getServings() ;
-        int recipeId = Integer.valueOf(mRecipesList.get(position).getId());
-
-
-        holder.name.setText(name);
-//        holder.servings.setText(context.getString(R.string.servings, servings));
+        holder.bind(mRecipesList.get(position));
     }
-
 
     @Override
     public int getItemCount() {
@@ -112,24 +104,41 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView name;
+        private TextView servings;
 
-        @BindView(R.id.recipe_name_tv)TextView name;
-        @BindView(R.id.servings_tv)TextView servings;
-
-        public ViewHolder(View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
+            name = itemView.findViewById(R.id.recipe_name_tv);
+            servings = itemView.findViewById(R.id.servings_tv);
             itemView.setOnClickListener(this);
-            ButterKnife.bind(this,itemView);
+            ButterKnife.bind(this, itemView);
         }
 
         @Override
         public void onClick(View view) {
-            listener.onRecipeCardClicked(view, getAdapterPosition());
+            if (listener != null) {
+                listener.onRecipeCardClicked(view, getAdapterPosition());
+            }
+        }
+
+        void bind(Recipes recipes) {
+            name.setText(recipes.getName());
+            String servingsText = servings.getContext().getString(R.string.servings, Integer.valueOf(recipes.getServings()));
+            servings.setText(servingsText);
+        }
+    }
+
+    @Nullable
+    public Recipes getItem(int position) {
+        if (position > mRecipesList.size() || position < 0) {
+            return null;
+        } else {
+            return mRecipesList.get(position);
         }
     }
 
     public interface RecipeCardClickListener{
-
         /**
          * Method for when recipe is clicked
          *
@@ -139,8 +148,7 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
         void onRecipeCardClicked(View view, int position);
     }
 
-    /**
-     * Custom click listener
-     */
-    public void setOnItemClickListener(RecipeCardClickListener listener){this.listener = listener;}
+    public void setOnItemClickListener(RecipeCardClickListener listener) {
+        this.listener = listener;
+    }
 }
